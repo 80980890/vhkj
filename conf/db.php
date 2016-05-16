79e48e39cecb906bf40aa26d5404362658bfeb17
pastime <80980890@qq.com>
2016-05-16 16:53:35 +0800

    init

---
+++ conf/db.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_PORT'=>'3306',
+	'DB_NAME'=>'78shangmen',
+	'DB_USER'=>'78shangmen',
+	'DB_PWD'=>'2G4TSbcLbzhAouHtI9UIcO0wWOFZ02xxxxxxxxxx',
+	'DB_PREFIX'=>'pigcms_',
+);
